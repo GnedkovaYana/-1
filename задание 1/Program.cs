c26c2a1d139aa7408ed4ba76fb9fa31aae8f67c2
Gnedkova Yana <ygnedkova@gmail.com>
2022-10-17 17:17:43 +0500

    Добавьте файлы проекта.

---
+++ задание 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace ex1
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            float comparson = float.NaN;
+            while (true)
+            {
+                Console.WriteLine("Введите число: ");
+                string number = Console.ReadLine();
+
+                bool intNumber = int.TryParse(number, out int intNum);
+                if (intNumber)
+                {
+                    Console.WriteLine((char)intNum);
+                    break;
+                }
+                bool floatnumber = float.TryParse(number, out float floNum);
+                if (floatnumber)
+                {
+                    if (floNum == comparson)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        comparson = floNum;
+                    }
+                }
+                else if (number == "q")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
